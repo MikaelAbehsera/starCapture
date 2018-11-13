910ca46c046f5bc633e2e7329227a8b10954f6dc
Mikael Abehsera <Mikael.abehsera@gmail.com>
2018-11-13 13:20:44 -0500

    added group colision, added player/star collision, stars have a global func that will remove them on colision

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ function preload() {
 
 var platforms;
 var player;
+var cursors;
+var stars;
 
 function create() {
   // add sky png as backround
@@ -50,6 +52,7 @@ function create() {
 
   //// all the player aka "dude" code ////
 
+
   // add dynamic physics to player body // player has 9 frames total ==> 4 for running left ==> 1 for facing the camera ==> 4 for running right
   player = this.physics.add.sprite(100, 450, "dude");
 
@@ -58,8 +61,10 @@ function create() {
   // don't allow player to leave screen so add colition with world borders to true
   player.setCollideWorldBounds(true);
 
+  // running left animation 
   this.anims.create({
     key: "left",
+    // frames 0-3 for left
     frames: this.anims.generateFrameNumbers("dude", {
       start: 0,
       end: 3
@@ -68,6 +73,7 @@ function create() {
     repeat: -1
   });
 
+  // Front/turning sprite 
   this.anims.create({
     key: "turn",
     frames: [{
@@ -77,8 +83,10 @@ function create() {
     frameRate: 20
   });
 
+  // running right animation 
   this.anims.create({
     key: "right",
+    // frames 0-3 for right
     frames: this.anims.generateFrameNumbers("dude", {
       start: 5,
       end: 8
@@ -86,6 +94,64 @@ function create() {
     frameRate: 10,
     repeat: -1
   });
+
+  stars = this.physics.add.group({
+    key: "star",
+    repeat: 11,
+    setXY: {
+      x: 12,
+      y: 0,
+      stepX: 70
+    }
+  });
+
+  stars.children.iterate(function (child) {
+
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+  });
+
+  // add a collision relation between player and platforms
+  this.physics.add.collider(player, platforms);
+  // add a collision relation between starts and platforms
+  this.physics.add.collider(stars, platforms);
+  //if user and star collide set star to null
+  this.physics.add.overlap(player, stars, collectStar, null, this);
+
+
+
 }
 
-function update() {}
+function update() {
+  cursors = this.input.keyboard.createCursorKeys();
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play("right", true);
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play("turn");
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
+
+//GLOBAL FUNCTIONS FOR REUSING
+/**
+ * if user and star collide, remove star from world
+ * @param {*} player player object
+ * @param {*} star star object
+ */
+function collectStar(player, star) {
+  star.disableBody(true, true);
+  console.log(player);
+
+}
